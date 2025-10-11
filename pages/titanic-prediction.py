f01c89f2e218b0f94103e2e7b5cb089df014eed5
Chiyue-Wang <wangchiyue169@gmail.com>
2025-10-11 11:22:15 +0800

    Update titanic-prediction.py

--- pages/titanic-prediction.py
+++ pages/titanic-prediction.py
@@ -11,7 +11,7 @@ model = load_model()
 
 st.title("Titanic Survival Prediction")
 
-st.subheader("请输入乘客信息进行生存概率预测：")
+st.subheader("Please enter the passenger information for survival probability prediction:")
 
 # 输入控件
 pclass = st.selectbox("Passenger Class (Pclass)", [1, 2, 3])
@@ -23,6 +23,7 @@ fare = st.number_input("Fare", min_value=0.0, max_value=600.0, value=32.0)
 sex_encoded = 1 if sex == "male" else 0
 X = np.array([[pclass, sex_encoded, age, fare]])
 
-if st.button("预测生存概率"):
+if st.button("Forecast survival probability"):
     prob = model.predict_proba(X)[0][1]  # [0][1] 表示生存的概率
-    st.success(f"预测生存概率为：{prob*100:.2f}%")
+
+    st.success(f"Forecast survival probability is：{prob*100:.2f}%")
