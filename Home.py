58bd3345d8c628c655beaade5bc2a7bed41b3303
Chiyue-Wang <wangchiyue169@gmail.com>
2025-10-11 11:24:51 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
 def main():
-    st.set_page_config(page_title="Welcome to the Titanic Model App", page_icon="🚢")
+    st.set_page_config(page_title="Welcome to the Titanic App 🚢", page_icon="🚢")
     st.title("Titanic Model App")
     st.write(
         """
-        Welcome to the Titanic Model Streamlit Application!  
-        Use the button below to navigate to the main app page for model predictions and analysis.
+        This simple web app allows you to **analyze** the Titanic dataset  
+        and **predict** passenger survival probability using a trained model.
         """
     )
-    if st.button("Go to App"):
+    if st.button("Go to Titanic App"):
         st.switch_page("pages/app.py")
-    if st.button("Go to Prediction"):
+    if st.button("Go to Titanic Prediction"):
         st.switch_page("pages/titanic-prediction.py")
 
 if __name__ == "__main__":
